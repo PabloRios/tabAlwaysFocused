b3812b4b9991576b72d16903a07b462dab473353
pablorios <PabloRios@users.noreply.github.com>
2024-10-31 12:46:48 -0300

    ammend

--- tabAlwaysFocused.js
+++ tabAlwaysFocused.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ALWAYS FOCUS
 // @namespace    https://github.com/PabloRios/tabAlwaysFocused
-// @version      2024-10-31
+// @version      0.00
 // @description  Maintain an artificial focus on browser tabs.
 // @author       pablorios
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
