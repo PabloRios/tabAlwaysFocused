27f3aa914a15ab2b71ac897adee4aa8a5460973d
pablorios <PabloRios@users.noreply.github.com>
2024-10-31 12:44:32 -0300

    ammend

--- tabAlwaysFocused.js
+++ tabAlwaysFocused.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         ALWAYS FOCUS
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/PabloRios/tabAlwaysFocused
 // @version      2024-10-31
-// @description  try to take over the world!
+// @description  Maintain an artificial focus on browser tabs.
 // @author       pablorios
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
@@ -10,10 +10,9 @@
 
 (function() {
     'use strict';
-    document.addEventListener("visibilitychange", onVisibilityChange);
-    function onVisibilityChange (e) {
+    document.addEventListener("visibilitychange", (e) => {
         console.log(e);
         Object.defineProperty(document, "hidden", { value : false });
-    }
+    });
 })();
 
